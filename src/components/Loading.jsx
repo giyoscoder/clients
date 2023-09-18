d5c60df3b4d435ce2daeb4d6ace0fe9554a61458
giyos <rashidovgiyos409@gmail.com>
2023-09-18 15:06:02 +0500

    a

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,7 +5,7 @@ import './Loading.css'
 
 const Loading = () => {
   return (
-    <div className="bg-white/60 h-[105%] w-full flex justify-center items-center absolute   top-0 left-0">
+    <div className="bg-white/60 h-[1000%] w-full flex justify-center items-center absolute   top-0 left-0">
       <NewtonsCradle size={50} speed={1.4} color="blue" />
     </div>
   );
